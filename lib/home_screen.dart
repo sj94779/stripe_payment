fa47c0907ab23f72e333e3415206457019c923e2
sj94779 <sj94779@gmail.com>
2023-10-09 15:38:45 +0530

    First Commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,109 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Map<String, dynamic>? paymentIntentData;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stripe Payment'),
+      ),
+      body: Center(
+        child: InkWell(
+          onTap: () async {
+            await makePayment();
+          },
+          child: Container(
+            height: 50,
+            width: 200,
+            color: Colors.green,
+            child: const Center(
+              child: Text(
+                'Pay',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> makePayment() async {
+    try {
+      paymentIntentData = await createPaymentIntent('20', 'USD');
+
+      await Stripe.instance
+          .initPaymentSheet(
+              paymentSheetParameters: SetupPaymentSheetParameters(
+                  setupIntentClientSecret:
+                      'sk_test_51NzFn7SHS1wPoIePJ3VmR9DJ9LxQwuxEfauuHn0pxgpPA2WOSETagc0oGDfFeMHj4sAjwU62hB4AJo9O7kGOP8Kp004uCc4uu3',
+                  paymentIntentClientSecret:
+                      paymentIntentData!['client_secret'],
+                  customFlow: true,
+                  style: ThemeMode.dark,
+                  merchantDisplayName: 'sj'))
+          .then((value) {});
+
+      displayPaymentSheet();
+    } catch (e, s) {
+      print('Payment exception:$e$s');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet().then((newValue) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("paid successfully")));
+
+        paymentIntentData = null;
+      }).onError((error, stackTrace) {
+        print('Exception/DISPLAYPAYMENTSHEET==> $error $stackTrace');
+      });
+    } on StripeException catch (e) {
+      print('Exception==> $e');
+    } catch (e) {
+      print('$e');
+    }
+  }
+
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': calculateAmount('20'),
+        'currency': currency,
+        'payment_method_types[]': 'card',
+      };
+      print(body);
+      var response = await http.post(
+          Uri.parse('https://api.stripe.com/v1/payment_intents'),
+          body: body,
+          headers: {
+            'Authorization': 'Bearer ' +
+                'sk_test_51NzFn7SHS1wPoIePJ3VmR9DJ9LxQwuxEfauuHn0pxgpPA2WOSETagc0oGDfFeMHj4sAjwU62hB4AJo9O7kGOP8Kp004uCc4uu3',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          });
+      print('Create Intent reponse ===> ${response.body.toString()}');
+      return jsonDecode(response.body);
+    } catch (err) {
+      print('err charging user: ${err.toString()}');
+    }
+  }
+
+  calculateAmount(String amount) {
+    final a = (int.parse(amount)) * 100;
+    return a.toString();
+  }
+}
